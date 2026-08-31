036973fdbdfbf8ff7e3a2c4ee6dda5d0a92879e3
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-31 01:32:23 -0600

    2026-08-31 01:30 CST There are now Opt/Req interfaces and implementations for the KeyHash types in server.markhome.mcf.v3_1.cflib.keyhash.*KeyHash*.java. The pom.xml attempts to build the code using the latest community edition of GraalVM (graalvm-community-25.3.4.1+1.1), but although I've finally worked the kinks out for the process, there still seem to be some missing 'gotchas' to be resolved before I can get GraalVM to do what I want it to. I've got a lot of command-line arguments specified in the pom.xml for Graal for cflib; I'm hoping to find a consistent approach to running a GraalVM build alongside a Java build from the same source code at the same time...

---
+++ src/main/java/server/markhome/mcf/v3_1/cflib/keyhash/CFLibOptKeyHash512.java
@@ -0,0 +1,64 @@
+/*
+ *	Mark's Code Fractal CFLib KeyHash 3.1 Common Library
+ *
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *
+ *	CFLib KeyHash provides essential types and utilities for Code Fractal applications.
+ *
+ *	These files are part of Mark's Code Fractal CFLib KeyHash.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later with static linking exception.
+ *
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the LGPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-LGPLv3-compatibly licensed code.  That said,
+ *	code which does not rely on this library is free to specify whatever license its
+ *	authors decide to use. Mark Sobkow specifically rejects the infectious nature of
+ *	the LGPLv3, and considers the mere act of including LGPLv3 modules in an
+ *	executable to be perfectly reasonable given tools like modern Java's single-jar
+ *	deployment options.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Library General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFLib KeyHash.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+package server.markhome.mcf.v3_1.cflib.keyhash;
+
+/**
+ * CFLibOptKeyHash512 extends CFLibKeyHash512 with the appropriate behavior for the isNull() and setNull() method signatures.
+ *
+ * @author msobkow
+ */
+public class CFLibOptKeyHash512 extends CFLibKeyHash512 implements ICFLibOptional, ICFLibOptKeyHash512 {
+
+	/**
+	 *	Make this value null.
+	 *
+	 *	@throws CFLibNullArgumentException
+	 */
+	@Override
+	public void setNull() {
+		byte[] ba = getBytes();
+		if (ba != null ) {
+			for( int i = 0; i < ba.length; i++ ) {
+				ba[i] = 0;
+			}
+		}
+	}
+}
---
+++ src/main/java/server/markhome/mcf/v3_1/cflib/keyhash/CFLibReqKeyHash224.java
@@ -0,0 +1,74 @@
+/*
+ *	Mark's Code Fractal CFLib KeyHash 3.1 Common Library
+ *
+ *	Copyright 2016-2026 Mark Stephen Sobkow
+ *
+ *	CFLib KeyHash provides essential types and utilities for Code Fractal applications.
+ *
+ *	These files are part of Mark's Code Fractal CFLib KeyHash.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later with static linking exception.
+ *
+ *	As a special exception, Mark Sobkow gives you permission to link this library
+ *	with independent modules to produce an executable, provided that none of them
+ *	conflict with the intent of the LGPLv3; that is, you are not allowed to invoke
+ *	the methods of this library from non-LGPLv3-compatibly licensed code.  That said,
+ *	code which does not rely on this library is free to specify whatever license its
+ *	authors decide to use. Mark Sobkow specifically rejects the infectious nature of
+ *	the LGPLv3, and considers the mere act of including LGPLv3 modules in an
+ *	executable to be perfectly reasonable given tools like modern Java's single-jar
+ *	deployment options.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Mark's Code Fractal CFLib KeyHash is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Library General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFLib KeyHash.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+package server.markhome.mcf.v3_1.cflib.keyhash;
+
+import server.markhome.mcf.v3_1.cflib.*;
+
+/**
+ * CFLibReqKeyHash224 extends CFLibKeyHash224 with the appropriate behavior for the isNull() and setNull() method signatures.
+ *
+ * @author msobkow
+ */
+public class CFLibReqKeyHash224 extends CFLibKeyHash224 implements ICFLibRequired, ICFLibReqKeyHash224 {
+
+	/**
+	 *	Is this value null?
+	 *
+	 *	@throws CFLibInvalidStateException if the superclass implementation of isNull() returns true.
+	 */
+	@Override
+	public boolean isNull() {
+		if (super.isNull()) {
+			throw new CFLibInvalidStateException(getClass(), "isNull", 0, "super.isNull()", "superclass value is not allowed to be null", null);
+		}
+		return(false);
+	}
+
+	/**
+	 *	Make this value null.
+	 *
+	 *	@throws CFLibNullArgumentException
+	 */
+	@Override
+	public void setNull() {
+		throw new CFLibNullArgumentException(getClass(), "setNull", 0, "required-attribute");
+	}
+}
