94d7caa808655064edb16f1aaad7f6510d4ef1c5
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-31 04:41:34 -0600

    2026-08-31 04:45 CST Okay, so now the compile phase is building the .class files in preparation. Still getting failures from GraalVM; I suspect because I need to cross-compile the dependencies and package them up.

--- src/main/java/server/markhome/mcf/v3_1/cflib/CFLibLibraryEntry.java
+++ src/main/java/server/markhome/mcf/v3_1/cflib/CFLibLibraryEntry.java
@@ -1,15 +1,15 @@
 package server.markhome.mcf.v3_1.cflib;
 
-import org.teavm.jso.JSExport;
-
 public class CFLibLibraryEntry {
 
-	@JSExport
+	public static int main(int argc, String[] argv) {
+		return 0;
+	}
+
 	public static String getCFLibName() {
 		return ("server.markhome.mcf.v3_1.cflib");
 	}
 
-	@JSExport
 	public static String getCFLibVersion() {
 		return ("3.1.42");
 	}
